f34da9c497cd2c734fd90082986d6d89e9b73a36
Marcin Orzech <marcin.w.orzech@gmail.com>
2024-07-30 16:24:06 +0100

    finished energy calculation method

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ Created on 27/07/2024
 # Import Python Libraries
 import pandas as pd
 import streamlit as st
-import plotly.express as px
+# import plotly.express as px
 from cell_components import materials, Electrode, Separator, Electrolyte, Pouch, Tab, Cell
 
 
@@ -18,7 +18,7 @@ def page_config():
     '''Setups page settings and menu options.
     Must be called as first streamlit command.'''
     st.set_page_config(
-        page_title='WattCalc',
+        page_title='WattCell',
         page_icon='assets/favicon.ico',
         layout='wide',
         menu_items={
@@ -27,45 +27,45 @@ def page_config():
     )
 
 
-def input_values():
+def design_cell():
 
     st.header('Cell Properties:')
-    c1,c2,c3,c4 = st.columns(4)
+    c1,c2,c3, c4 = st.columns(4)
 
     with c1:
         st.write('### Cathode:')
         cathode_am = st.selectbox('Cathode active material',['Prussian Blue'])
         cathode_binder = st.selectbox('Binder type',['PVDF', 'CMC+SBR'])
         porosity = st.slider('Porosity (%)', 0, 100, value=25)/100
-        voltage = st.number_input('Voltage (V)',0.0,value=materials[cathode_am]['voltage'])
+        voltage = st.slider('Voltage (V)',1.0, 5.0, step = 0.05, value=materials[cathode_am]['voltage'])
         capacity = st.number_input('Capacity (Ah/kg)',0,value=materials[cathode_am]['capacity'])
         density_am = st.number_input('Active material density',0.0,value=materials[cathode_am]['density'])
-        thickness = st.number_input('Cathode thickness (um)', 0, value=25)/10000
+        thickness = st.slider('Cathode thickness (um)', 0, 150, value=80)
         width = st.number_input('Cathode width (mm)', 0, value=43)
         height = st.number_input('Cathode height (mm)', 0, value=56)
         cathode_cc = st.selectbox('Cathode current collector',['Al', 'Cu'])
         cathode_cc_thickness = st.number_input('Current collector thickness (um)',
-                                                value=materials[cathode_cc]['thickness'])/10000
+                                                value=materials[cathode_cc]['thickness'])
 
 
         st.write('#### Mass ratios:')
-        c_am = st.number_input('AM', 0, 100, value=96)/100
-        c_carbon = st.number_input('carbon', 0, (100-int(c_am*100)), value=(100-int(c_am*100)))/100
-        c_binder = st.number_input('binder', value=int((1 - c_am - c_carbon)*100), disabled=True)/100
+        c_am = st.number_input('AM', 0, 100, value=96)
+        c_carbon = st.number_input('carbon', 0, (100-c_am), value=(100-c_am))
+        c_binder = st.number_input('binder', value=int(100 - c_am - c_carbon), disabled=True)
 
     cathode = Electrode(
         active_material=cathode_am,
-        mass_ratio={'am': c_am, 'carbon': c_carbon, 'binder': c_binder},
+        mass_ratio={'am': c_am/100, 'carbon': c_carbon/100, 'binder': c_binder/100},
         binder=cathode_binder,
         porosity=porosity,
         voltage=voltage,
         capacity=capacity,
         density_am=density_am,
-        width=width,
-        height=height,
-        thickness=thickness,
+        width=width/10,
+        height=height/10,
+        thickness=thickness/10000,
         current_collector=cathode_cc,
-        cc_thickness=cathode_cc_thickness
+        cc_thickness=cathode_cc_thickness/10000
     )
 
     with c2:
@@ -73,41 +73,42 @@ def input_values():
         anode_am = st.selectbox('Anode active material',['Hard Carbon'])
         anode_binder = st.selectbox('Binder type',['PVDF', 'CMC+SBR'], index=1, key='anode_binder')
         anode_porosity = st.slider('Porosity (%)', 0, 100, value=25, key='anode_por')/100
-        anode_voltage = st.number_input('Voltage (V)',0.0,value=materials[anode_am]['voltage'], key='anode_V')
+        anode_voltage = st.slider('Voltage (V)',0.0, 3.0, step = 0.05,value=materials[anode_am]['voltage'], key='anode_V')
         anode_capacity = st.number_input('Capacity (Ah/kg)',0,value=materials[anode_am]['capacity'], key='anode_cap')
         anode_density_am = st.number_input('Active material density',0.0,value=materials[anode_am]['density'], key='anode_am_d')
-        anode_thickness = st.number_input('Anode Thickness (um)', 0, value=25)/1000
-        st.info(f'Anode width (mm): {cathode.width + 2}')
-        st.info(f'Anode height (mm): {cathode.height + 2}')
+        placeholder = st.empty()
+        st.info(f'Anode width (mm): {cathode.width*10 + 2:.0f}')
+        st.info(f'Anode height (mm): {cathode.height*10 + 2:.0f}')
         anode_cc = st.selectbox('Anode current collector',['Al', 'Cu'])
         anode_cc_thickness = st.number_input('Current collector thickness (um)', 
                                             value=materials[anode_cc]['thickness'],
-                                            key='anode_cc_thickness')/10000
+                                            key='anode_cc_thickness')
 
         st.write('#### Mass ratios:')
-        a_am = st.number_input('AM', 0, 100, value=96, key='anode_am')/100
-        a_carbon = st.number_input('carbon', 0, (100-int(a_am*100)), value=(100-int(a_am*100)), key='anode_c')/100
-        a_binder = st.number_input('binder', value=int((1 - a_am - a_carbon)*100), key='anode_b', disabled=True)/100
+        a_am = st.number_input('AM', 0, 100, value=96, key='anode_am')
+        a_carbon = st.number_input('carbon', 0, (100-a_am), value=(100-a_am), key='anode_c')
+        a_binder = st.number_input('binder', value=int(100 - a_am - a_carbon), key='anode_b', disabled=True)
 
     anode = Electrode(
         active_material=anode_am,
         current_collector=anode_cc,
-        mass_ratio={'am': a_am, 'carbon': a_carbon, 'binder': a_binder},
+        mass_ratio={
+            'am': a_am/100, 'carbon': a_carbon/100, 'binder': a_binder/100
+            },
         binder=anode_binder,
         porosity=anode_porosity,
         voltage=anode_voltage,
         capacity=anode_capacity,
         density_am=anode_density_am,
-        width=cathode.width + 2,
-        height=cathode.height + 2,
-        thickness=anode_thickness,
-        cc_thickness=anode_cc_thickness
+        width=cathode.width + 0.2,
+        height=cathode.height + 0.2,
+        cc_thickness=anode_cc_thickness/10000
     )
 
     with c3:
         st.write('### Separator:')
         separator_name = st.selectbox('Separator name', ['Celgard 2325'])
-        separator_thickness = st.number_input('Separator thickness (um)', value=materials[separator_name]['thickness']) / 1000
+        separator_thickness = st.number_input('Separator thickness (um)', value=materials[separator_name]['thickness'])
         separator_porosity = st.slider('Separator porosity (%)', 0, 100,
                                         value=int(materials[separator_name]['porosity']*100)) / 100
         separator_density = st.number_input('Separator density (g/cm³)',
@@ -116,87 +117,114 @@ def input_values():
     separator = Separator(
         material=separator_name,
         width=anode.width,
-        height=anode.height + 2,
-        thickness=separator_thickness,
+        height=anode.height + 0.2,
+        thickness=separator_thickness/10000,
         porosity=separator_porosity,
         density=separator_density
     )
 
     with c3:
+        '---'
         st.write('### Electrolyte:')
         electrolyte_type = st.selectbox('Electrolyte type', materials['electrolytes'].keys())
-        electrolyte_density = st.number_input('Electrolyte density (g/cm³)', 
-                                            value=materials['electrolytes'][electrolyte_type]['density'])
-        
-        electrolyte = Electrolyte(
-        material=electrolyte_type,
-        density=electrolyte_density
-    )
+        electrolyte_density = st.number_input(
+            'Electrolyte density (g/cm³)',
+            value=materials['electrolytes'][electrolyte_type]['density'])
+        electrolyte_excess = st.slider('Excess electrolyte (%)', -20, 50, value=0)/100
+        calc_elec = st.empty()
 
-    with c3:
+        electrolyte = Electrolyte(
+            material=electrolyte_type,
+            density=electrolyte_density,
+            volume_excess=electrolyte_excess
+        )
+
+    with c4:
+        st.write('### Cell Configuration:')
+        layers_number = st.slider('Number of layers', 1, 30, value=20, step=1)
+        n_p_ratio = st.slider('N/P Ratio', 0.0, 1.5, value=1.1, step=0.05)
+        ice = st.slider('Initial Coulombic Efficiency (%)', 50, 100, value=93) / 100
+        
+        '---'
         st.write('### Pouch:')
-        pouch_thickness = st.number_input('Pouch thickness (um)', value=materials['pouch']['thickness']) / 1000
+        pouch_thickness = st.number_input('Pouch thickness (um)', value=materials['pouch']['thickness'])
         pouch_density = st.number_input('Pouch density (g/cm³)', value=materials['pouch']['density'])
 
-        st.write('### Tabs properties:')
-        tabs_material = st.selectbox('Tabs material', materials['tabs'].keys())
+        '---'
+        st.write('### Tabs:')
+        tabs_material_cathode = st.selectbox('Cathode tab material', materials['tabs'].keys())
+        tabs_material_anode = st.selectbox('Anode tab material', materials['tabs'].keys())
         tabs_height = st.number_input('Tabs height (mm)', value=80)
         tabs_width = st.number_input('Tabs width (mm)', value=8)
-        tabs_thickness = st.number_input('Tabs thickness (mm)',
-                                        value=materials['tabs'][tabs_material]['thickness']) / 10
+        tabs_thickness = st.number_input('Tabs thickness (mm)', value=0.2)
         
 
     pouch = Pouch(
         width=separator.width + materials['pouch']['extra_width'],
         height=separator.height + materials['pouch']['extra_height'],
-        thickness=pouch_thickness,
+        thickness=pouch_thickness / 10000,
         density=pouch_density
     )
     tabs = Tab(
-        material=tabs_material,
-        height=tabs_height,
-        width=tabs_width,
-        thickness=tabs_thickness
+        material_cathode=tabs_material_cathode,
+        material_anode=tabs_material_anode,
+        height=tabs_height/10,
+        width=tabs_width/10,
+        thickness=tabs_thickness/10
     )
 
-    df_cell = pd.DataFrame([cathode, anode, separator, electrolyte, pouch, tabs],
-                        index=['cathode', 'anode', 'separator', 'electrolyte', 'pouch', 'tabs']).T
+    designed_cell = Cell(cathode, anode, separator, electrolyte, pouch, tabs,
+                        layers_number, n_p_ratio, ice)
+    # df_cell = pd.DataFrame([cathode, anode, separator, electrolyte, pouch, tabs],
+    #                     index=['cathode', 'anode', 'separator', 'electrolyte', 'pouch', 'tabs']).T
     
-    st.dataframe(df_cell,use_container_width=True)
-
-    battery = Cell(cathode, anode, separator, electrolyte, pouch, tabs)
-
-    return battery
-
-def backend_calc():
-    '''
-    calculate intermidiate results here
-    mass of each item
-    volume of each item
-    volume ratios
-    electrodes density
-    electrodes volume (coated)
-    area of electrodes
-    mass of electrolyte
-    total void volume
-    '''
-
-def calculate_energy(inputs):
-    '''
-    calculation of final values based in background calculations and inputs
-    results:
-    total mass
-    total volume
-    capacity
-    energy
-    specific energy
-    energy density
-
-    '''
-    cell = inputs
+    # st.dataframe(df_cell, use_container_width=True)
+    placeholder.info(f'Anode Thickness (um): {anode.thickness*10000:.0f}')
+    with calc_elec.container():
+        st.info(f"Volume: {electrolyte.volume:.2f} mL")
+        st.info(f"Volume per Ah: {electrolyte.volume_per_ah:.2f} mL/Ah")
 
 
+
+    return designed_cell
+
+
+# def calculate_energy(cell_params):
+#     cell = cell_params
+#     cell.calculate_energy_density()
+
+def print_cell_metrics(cell):
+
+    st.write('---')
+    st.header('Calculated Cell Performance:')
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.metric("Total Mass", f"{cell.total_mass:.2f} g")
+        st.metric("Total Volume", f"{cell.total_volume:.2f} cm³")
+    
+    with col2:
+        st.metric("Capacity", f"{cell.capacity:.2f} Ah")
+        st.metric("Energy", f"{cell.energy:.2f} Wh")
+
+    with col3:
+        st.metric("Specific Energy", f"{cell.gravimetric_energy_density:.2f} Wh/kg")
+        st.metric("Energy Density", f"{cell.volumetric_energy_density:.2f} Wh/cm³")
+    
+    with st.sidebar:
+        st.write('# Calculated Cell Performance:')
+
+        st.metric("Total Mass", f"{cell.total_mass:.2f} g")
+        st.metric("Total Volume", f"{cell.total_volume:.2f} cm³")
+
+        st.metric("Capacity", f"{cell.capacity:.2f} Ah")
+        st.metric("Energy", f"{cell.energy:.2f} Wh")
+
+        st.metric("Specific Energy", f"{cell.gravimetric_energy_density:.2f} Wh/kg")
+        st.metric("Energy Density", f"{cell.volumetric_energy_density:.2f} Wh/cm³")
 
 page_config()
-input_values()
-# calculate_energy(input_values())
+battery = design_cell()
+# calculate_energy(battery)
+print_cell_metrics(battery)
+
--- cell_components.py
+++ cell_components.py
@@ -32,12 +32,11 @@ materials = {
     'pouch': {
         'thickness': 113,  # in um
         'density': 1.62,  # in g/cm³
-        'extra_width': 9,  # in mm
-        'extra_height': 20  # in mm
+        'extra_width': 0.9,  # in cm
+        'extra_height': 2  # in cm
     },
     'tabs': {
         'Al': {
-            'thickness': 0.2,  # in mm
             'density': 2.7
         }
     },
@@ -59,15 +58,17 @@ class Electrode:
     density_am: float  # g/cm3
     width: float  # cm
     height: float  # cm
-    thickness: float  # cm
     cc_thickness: float  # cm
     current_collector: str
+    thickness: float = 0 # cm
     tab_height: float = 1  # cm
     tab_width: float = 1  # cm
     density: float = field(init=False)
+    areal_capacity: float = field(init=False)  # mAh/cm²
 
     def __post_init__(self):
         self.calculate_composite_density()
+        self.calculate_areal_capacity()
 
     def calculate_composite_density(self):
         volumes = {
@@ -82,12 +83,15 @@ class Electrode:
             volume_ratios['binder'] * materials[self.binder]['density']
         )
 
+    def calculate_areal_capacity(self):
+        self.areal_capacity = self.density * self.thickness * self.capacity * self.mass_ratio['am']
+
 @dataclass
 class Separator:
     material: str
-    width: float
-    height: float
-    thickness: float
+    width: float  # cm
+    height: float  # cm
+    thickness: float  # cm
     porosity: float
     density: float
 
@@ -95,23 +99,30 @@ class Separator:
 class Electrolyte:
     material: str
     density: float
+    volume_excess: float = 0
+    volume: float = field(init=False)
+    volume_per_ah: float = field(init=False)
 @dataclass
 class Pouch:
-    width: float
-    height: float
-    thickness: float # cm
+    width: float  # cm
+    height: float  # cm
+    thickness: float  # cm
     density: float
 
 @dataclass
 class Tab:
-    material: str
-    height: float
-    width: float
+    material_cathode: str
+    material_anode: str
+    height: float  # cm
+    width: float  # cm
     thickness: float # cm
-    density: float = field(init=False)
+    density_cathode: float = field(init=False)
+    density_anode: float = field(init=False)
 
     def __post_init__(self):
-        self.density = materials.get(self.material)['density']
+        self.density_cathode = materials.get(self.material_cathode)['density']
+        self.density_anode = materials.get(self.material_anode)['density']
+
 
 @dataclass
 class Cell:
@@ -121,8 +132,91 @@ class Cell:
     electrolyte: Electrolyte
     format: Pouch
     tabs: Tab
+    layers_number: int
+    n_p_ratio: float
+    ice: float = 0.93
+    
+    # attributes to store calculation results
+    volumetric_energy_density: float = field(init=False)
+    gravimetric_energy_density: float = field(init=False)
+    energy: float = field(init=False)
+    capacity: float = field(init=False)
+    total_mass: float = field(init=False)
+    total_volume: float = field(init=False)
+
+
+    def __post_init__(self):
+        self.calculate_anode_properties()
+        self.calculate_energy_density()
+
+    def calculate_anode_properties(self):
+        required_anode_capacity = self.cathode.areal_capacity * self.n_p_ratio
+        
+        # Calculate required anode thickness
+        self.anode.thickness = required_anode_capacity / (
+            self.anode.density * self.anode.capacity * self.anode.mass_ratio['am'])
+        
+        # Recalculate anode areal capacity
+        self.anode.calculate_areal_capacity()
 
     def calculate_energy_density(self):
-        # Implement energy density calculation here
-        pass
+        '''
+        calculation of final values inputs
+        results:
+        total mass
+        total volume
+        capacity
+        energy
+        specific energy
+        energy density
+        '''
+        # Calculate volumes of individual item (cm3) 
+        cathode_volume = self.cathode.width * self.cathode.height * self.cathode.thickness* 2 * self.layers_number
+        anode_volume = self.anode.width * self.anode.height * self.anode.thickness * (2 * self.layers_number + 2)  # Extra anode layer
+        separator_volume = self.separator.width * self.separator.height * self.separator.thickness * 2 * self.layers_number
+        pouch_volume = self.format.width * self.format.height * self.format.thickness * 2
+        anode_cc_volume = (self.layers_number + 1)* (  # Extra anode current collector
+            self.anode.width * self.anode.height + self.anode.tab_height + self.anode.tab_width
+            ) * self.anode.cc_thickness
+        cathode_cc_volume = self.layers_number * (
+            self.cathode.width * self.cathode.height + self.cathode.tab_height * self.cathode.tab_width
+            ) * self.cathode.cc_thickness
 
+        # Calculate masses (g)
+        cathode_mass = cathode_volume * self.cathode.density
+        anode_mass = anode_volume * self.anode.density
+        separator_mass = separator_volume * self.separator.density
+        pouch_mass = pouch_volume * self.format.density
+        cathode_cc_mass = cathode_cc_volume * materials[self.cathode.current_collector]['density']
+        anode_cc_mass = anode_cc_volume * materials[self.anode.current_collector]['density']
+        tabs_mass = self.tabs.height * self.tabs.width * self.tabs.thickness * (self.tabs.density_cathode + self.tabs.density_anode)
+
+        # Calculate void volume for electrolyte
+        cathode_void_volume = cathode_volume * self.cathode.porosity
+        anode_void_volume = anode_volume * self.anode.porosity
+        separator_void_volume = separator_volume * self.separator.porosity
+        total_void_volume = cathode_void_volume + anode_void_volume + separator_void_volume
+
+        # Calculate electrolyte mass and volume
+        self.electrolyte.volume = total_void_volume * (1 + self.electrolyte.volume_excess)
+        electrolyte_mass = self.electrolyte.volume * self.electrolyte.density
+
+        # Calculate total mass and volume
+        self.total_mass = cathode_mass + cathode_cc_mass + anode_mass + anode_cc_mass + separator_mass + pouch_mass + tabs_mass + electrolyte_mass
+        self.total_volume = cathode_volume + anode_volume + separator_volume + pouch_volume + (self.electrolyte.volume - total_void_volume) + anode_cc_volume + cathode_cc_volume
+
+        # Calculate capacity (based on the limiting electrode)
+        cathode_capacity = cathode_mass * self.cathode.mass_ratio['am'] * self.cathode.capacity / 1000  # Convert to Ah
+        anode_capacity = anode_mass * self.anode.mass_ratio['am'] * self.anode.capacity / 1000  # Convert to Ah
+        self.capacity = min(cathode_capacity, anode_capacity) * self.ice
+
+        # Calculate volume of electrolyte per Ah
+        self.electrolyte.volume_per_ah = self.electrolyte.volume / self.capacity  # cm³/Ah
+
+        # Calculate energy
+        cell_voltage = self.cathode.voltage - self.anode.voltage
+        self.energy = self.capacity * cell_voltage  # in Wh
+
+        # Calculate energy density and specific energy
+        self.volumetric_energy_density = self.energy / self.total_volume * 1000  # Wh/L
+        self.gravimetric_energy_density = self.energy / self.total_mass * 1000  # Wh/kg
